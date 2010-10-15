a313dfa4a85b8bc0b86fa7acea77c21fd2602251
neochrome <johan@stenqvist.net>
2010-10-15 09:39:09 +0200

    support for x64 windows added

--- register.fsx.bat
+++ register.fsx.bat
@@ -1,4 +1,7 @@
 @echo off
+setlocal
+if $"%ProgramW6432%" == $ set base=%ProgramFiles%
+if $"%ProgramW6432%" neq $ set base=%ProgramFiles(x86)%
 assoc .fsx=FSharpScript
-ftype FSharpScript="%programfiles%\Microsoft F#\v4.0\fsi.exe" "%%0" %%*
+ftype FSharpScript="%base%\Microsoft F#\v4.0\fsi.exe" "%%0" %%*
 setx PATHEXT .fsx;%PATHEXT%
