57b37b3cb5608907c1bfb3428fd7e5af6eac5585
neochrome <johan@stenqvist.net>
2010-12-02 14:48:23 +0100

    moved register.fsx.bat to settings

--- register.fsx.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-setlocal
-if $"%ProgramW6432%" == $ set base=%ProgramFiles%
-if $"%ProgramW6432%" neq $ set base=%ProgramFiles(x86)%
-assoc .fsx=FSharpScript
-ftype FSharpScript="%base%\Microsoft F#\v4.0\fsi.exe" "%%0" %%*
-setx PATHEXT .fsx;%PATHEXT%
