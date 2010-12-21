e36e59a9f4ef5026173e35fd7e4c1660a1b4bf82
neochrome <johan@stenqvist.net>
2010-12-21 09:25:45 +0100

    basic xslt transform script added

---
+++ scripts/test.xslt
@@ -0,0 +1,14 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform" xmlns:msxsl="urn:schemas-microsoft-com:xslt" exclude-result-prefixes="msxsl">
+	<xsl:output method="html" indent="yes" doctype-public="http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd" encoding="Windows-1252"/>
+	<xsl:param name="iteration" />
+
+	<xsl:template match="/">
+		<root>
+			<hello>
+				<world/>
+			</hello>
+		</root>
+	</xsl:template>
+	
+</xsl:stylesheet>
---
+++ scripts/xslt.fsx
@@ -0,0 +1,29 @@
+﻿open System
+open System.IO
+open System.Net
+open System.Xml
+open System.Xml.Xsl
+
+let transform (source:XmlReader) (how:XmlReader) (output:XmlWriter) (properties:seq<string*string>) =
+  let xslt = new XslCompiledTransform()
+  xslt.Load(how)
+  let args = Seq.fold (fun (acc:XsltArgumentList) (name,value) -> acc.AddParam(name, "", value);acc) (new XsltArgumentList()) properties
+  xslt.Transform(source, args, output)
+
+let usage(message) = 
+  printfn "%s\nUsage:\n%s <source.xml> <transform.xslt> <output.xml>" fsi.CommandLineArgs.[0] message
+  exit 1
+
+let args = fsi.CommandLineArgs
+if args.Length <> 4 then usage("Invalid number of arguments")
+
+using (XmlTextReader.Create(args.[1])) (fun source ->
+  using (XmlTextReader.Create(args.[2])) (fun how ->
+    let settings = new XmlWriterSettings()
+    settings.Indent <- true
+    settings.IndentChars <- "\t"
+    using (XmlTextWriter.Create(args.[3], settings)) (fun output ->
+      transform source how output Seq.empty
+    )
+  )
+)
