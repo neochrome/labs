a6a110af961213b824c5c466385e5feb15d2abd5
neochrome <johan@stenqvist.net>
2011-02-10 08:55:39 +0100

    added minimalistic namespace helper

---
+++ js/namespace.js
@@ -0,0 +1,13 @@
+﻿(function () {
+	window.namespace = function (namespace, declaration) {
+		var parts = namespace.split('.');
+		var context = window;
+		while (parts.length > 0) {
+			var part = parts.shift();
+			if (!context[part])
+				context[part] = {};
+			context = context[part];
+		}
+		declaration(context);
+	};
+})();
