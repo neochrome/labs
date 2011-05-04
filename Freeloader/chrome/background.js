dcbfdcab4673ab74004beeecef76f6910d7b8f5b
neochrome <johan@stenqvist.net>
2011-05-04 09:45:36 +0200

    freeloading

--- Freeloader/chrome/background.js
+++ Freeloader/chrome/background.js
@@ -1,16 +1,53 @@
+var stopped = true;
+var running = false;
+
 chrome.browserAction.onClicked.addListener(function(tab){
-	alert('hello');
+	running = !running;
+	if(stopped){
+		comet();
+	}
+	if(running){
+		icon.enabled();
+	} else {
+		icon.disabled();
+	}
 });
 
 var comet = function(){
 	$.get('http://localhost:1337/')
 		.success(function(data, status, xhr){
-			alert(data);
-			comet();
+			if(data !== ''){
+				chrome.windows.getCurrent(function(window){
+					chrome.tabs.getSelected(window.id, function(tab){
+						chrome.tabs.update(tab.id, {url:tab.url});
+					});
+				});
+			}
 		})
-	.error(function(){
-		alert('epic fail!');
-	});
+		.error(function(){
+			icon.error();
+		})
+		.complete(function(){
+			if(running){
+				comet();
+			} else {
+				stopped = true;
+			}
+		});
 };
 
-comet();
+var icon = {
+	enabled:function(){
+		this._set('icon.png');
+	},
+	disabled:function(){
+		this._set('icon-disabled.png');
+	},
+	error:function(){
+		this._set('icon-error.png');
+	},
+	_set:function(filename){
+		chrome.browserAction.setIcon({path:filename});
+	}
+};
+
