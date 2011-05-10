1dbf744c0736e7ef80f75baa764fe807197c2d2a
neochrome <johan@stenqvist.net>
2011-05-10 13:14:14 +0200

    TeamCity build status

--- TeamCity/chrome/background.js
+++ TeamCity/chrome/background.js
@@ -1,27 +1,24 @@
 
-//chrome.browserAction.onClicked.addListener(function(tab){
-//	chrome.browserAction.setPopup({popup:JSON.stringify(builds)});
-//chrome.browserAction.setBadgeText({text:number.toString()});
-//chrome.browserAction.setBadgeBackgroundColor({color:[(number % 16) * 16,0,0,255]});
-//});
+var composeIcon = function(badgeImageUrl, whenDone){
+	var canvas = document.getElementById('icon-canvas');
+	var ctx = canvas.getContext('2d');
+	ctx.clearRect(0,0,canvas.width, canvas.height);
 
-//var toast = function(message){
-//	var notification = webkitNotifications.createNotification('failure.gif', 'Build failed!', message);
-//	notification.show();
-//	setTimeout(function(){notification.cancel();}, 10000);
-//};
-
-//var failureCanvas = document.getElementById('failure');
-//var ctx = failureCanvas.getContext('2d');
-//var icon = new Image();
-//icon.src = 'icon128.png';
-//var icon = document.getElementById('icon');
-//ctx.drawImage(icon, 0, 0, 19, 19);
-//var badge = new Image();
-//badge.src = 'failure.gif';
-//ctx.drawImage(badge, 0, 0, 10, 10);
-//var failureIcon = ctx.getImageData(0, 0, 19, 19);
-
+	var icon = new Image();
+	icon.src = 'icon128.png';
+	icon.onload = function(){
+		ctx.drawImage(icon, 0, 0, 19, 19);
+		var badge = new Image();
+		badge.src = badgeImageUrl;
+		badge.onload = function(){
+			ctx.drawImage(badge, 5, 5, 15, 15);
+			whenDone(ctx.getImageData(0, 0, 19, 19));
+		};
+	};
+};
+var failureIconData, successIconData;
+composeIcon('success.gif', function(data){ successIconData = data; });
+composeIcon('failure.gif', function(data){ failureIconData = data; });
 
 var builds = [];
 
@@ -65,12 +62,15 @@ var update = function(){
 	};
 
 	var updateCompleted = function(){
-		var anyFailed = builds.some(function(x){ return x.status === 'failure'; });
-		if(anyFailed){
-			chrome.browserAction.setIcon({path:'failure.gif'});
-		} else {
-			chrome.browserAction.setIcon({path:'success.gif'});
-		}
+		var failed = 0;
+		builds.forEach(function(x){
+			if(x.status === 'failure'){ failed++; }
+	 	});
+		var iconData, badgeText;
+		iconData = failed ? failureIconData : successIconData;
+		badgeText = failed > 1 ? failed.toString() : '';
+		chrome.browserAction.setIcon({imageData:iconData});
+		chrome.browserAction.setBadgeText({text:badgeText});
 	};
 
 };
