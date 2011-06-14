1c513c85589d4a11feea79754d6bc6ff50eab52e
neochrome <johan@stenqvist.net>
2011-06-14 17:38:43 +0200

    experimenting with some speccing

---
+++ fspec/fspec.fsx
@@ -0,0 +1,40 @@
+﻿[<AutoOpen>]
+module Fspec
+open System
+exception private Fail
+exception private Pending of string
+
+let private verifyBehaviour description behaviour =
+  try
+    behaviour()
+    printfn "  - %s" description
+  with
+  | Fail -> printfn "F - %s" description
+  | Pending(why) -> printfn "P - %s (%s)" description why
+  | epic -> printfn "E - %s (%s)" description epic.Message
+
+let describe something behaves =
+  printfn "%s: " something
+  behaves()
+  printfn ""
+
+let it should behave =
+  verifyBehaviour should behave
+
+//let private format how what =
+//  let (|Int|_|) what =
+//    if Int32.
+
+
+let for_all should args behave = 
+  args |> Seq.iter (fun arg ->
+    let shouldText = System.String.Format(should, [arg])
+    verifyBehaviour shouldText (fun () -> behave arg)
+  )
+
+let pending why = raise(Pending(why))
+let fail() = raise(Fail)
+let private verify expectation = if not expectation then fail()
+let should what = what
+let be expected actual = verify (actual = expected)
+let not_be expected actual = verify (actual <> expected)
---
+++ fspec/specs.fsx
@@ -0,0 +1,48 @@
+﻿open System
+#load @"fspec.fsx"
+
+describe "specs, when executed" <| fun()->
+  it "should succeed" <| fun()->
+    ()
+
+  it "should fail" <| fun()->
+    fail()
+
+  it "should epic fail" <| fun()->
+    failwith "epic failure"
+
+  it "should be pending" <| fun()->
+    pending "since I said so"
+  
+describe "be: tests equality for" <| fun()->
+  it "numbers" <| fun()->
+    1 |> should be 1
+  it "text" <| fun()->
+    "one" |> should be "one"
+  it "instances" <| fun()->
+    let one = new Object()
+    one |> should be one
+
+describe "not_be: tests equality for" <| fun()->
+  it "numbers" <| fun()->
+    1 |> should not_be 2
+  it "text" <| fun()->
+    "one" |> should not_be "two"
+  it "instances" <| fun()->
+    let one = new Object()
+    let other = new Object()
+    one |> should not_be other
+
+describe "row input" <| fun()->
+  [2; 4; 6; 8]
+  |> for_all "test {0}" <| fun x ->
+      x % 2 |> should be 0
+  ['a';'b';'c']
+  |> for_all "test {0}" <| fun x ->
+      x |> should be x
+  ["apple";"banana";"clementine"]
+  |> for_all "test {0}" <| fun x ->
+      x |> should be x
+  [(1,1,2);(1,2,3)]
+  |> for_all "test {0}" <| fun (a,b,c) ->
+      a + b |> should be c
