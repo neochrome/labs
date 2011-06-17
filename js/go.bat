14be19125add699e18d786aaef902388e1960677
neochrome <johan@stenqvist.net>
2011-06-17 16:59:49 +0200

    better jslint output

--- js/go.bat
+++ js/go.bat
@@ -1,3 +1,3 @@
 @echo off
-tools\jsl.exe conf tools\jsl.default.conf -nofilelisting +recurse -process src/*.js
 node tools/jasmine-node/cli.js --color src
+tools\jsl.exe conf tools\jsl.default.conf -nologo -nofilelisting +recurse -process src/*.js
