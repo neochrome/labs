9e25dfd505edb7ecbb5040d9ad2f3562b7fdb7da
neochrome <johan@stenqvist.net>
2011-12-15 16:51:04 +0100

    more haxxors

--- tweetloader/server.js
+++ tweetloader/server.js
@@ -4,96 +4,101 @@ var url = require('url');
 var path = require('path');
 var fs = require('fs');
 var util = require('util');
+var EventEmitter = require('events').EventEmitter;
 var _ = require('underscore');
 var Twitter = require('ntwitter');
-var options = require('./options.js');
 
-var twitter = new Twitter(options);
-
-
-var state = { lastSeenId: null };
-twitter.getUserTimeline({max_id:state.lastSeenId}, function(err, data){
-});
-
-twitter.stream('user', function(stream){
-	stream.on('data', function(data){
-		if(typeof data.id_str === 'undefined'){
-			console.log(sys.inspect(data));
-			return;
-		}
-
-		// save last seen id
-		state.lastSeenId = data.id_str;
-		fs.writeFileSync('state.json', JSON.stringify(state), 'utf8');
-		console.log('state was saved:\n' + sys.inspect(state)); 
-
-		// do nothing if a reply or has no link
-		var tweet = new Tweet(data);
-		if(tweet.isResponse || !tweet.hasLink){
-			return;
-		}
-
-		// download the links from the message and respond when done
-		var dlOptions = url.parse(tweet.url);
-		dlOptions.path = dlOptions.pathname;
-		var filename = path.basename(dlOptions.path);
-		console.log('downloading \'' + tweet.url + '\' to: ' + filename);
-		http.get(dlOptions, function(res){
-			switch(res.statusCode){
-				case 200:
-					res.on('end', function(){
-						tweet.respond(twitter, 'done');
-						console.log('done downloading \'' + tweet.url + '\'');
-					});
-					var file = fs.createWriteStream(filename);
-					file
-						.once('open', function(fd){
-							util.pump(res, file, function(e){
-								tweet.respond(twitter, 'error');
-								console.log('error downloading \'' + tweet.url + '\'\n' + e.message);
-							});
-						});
-					break;
-				default:
-					tweet.respond(twitter, ['error', 'code' + res.statusCode]);
-					console.log('done downloading \'' + tweet.url + '\'');
-			}
-		}).on('error', function(e){
-			console.log('error downloading \'' + tweet.url + '\'\n' + e.message);
-		});
-
-	});
-	stream.on('end', function(res){
-		console.log('end: ');
-	});
-	stream.on('destroy', function(res){
-		console.log('destroy: ');
-	});
-});
 
 
 var Tweet = function(tweetData){
+	this.id = tweetData.id;
 	this.user = '@' + tweetData.user.screen_name;
-	this.id = tweetData.id_str;
-	this.hasLink = tweetData.entities.urls.length > 0;
-	if(this.hasLink){
-		this.url = tweetData.entities.urls[0].expanded_url;
-	}
-	this.isResponse = 
+	this.links = 
+		_(tweetData.entities.urls)
+		.map(function(url){ return url.expanded_url; });
+	this.isStatusUpdate = 
 		_(tweetData.entities.hashtags)
 		.chain()
 		.map(function(tag){ return tag.text; })
 		.any(function(tag){ return _(['done','error']).contains(tag); })
 		.value();
 };
-Tweet.prototype.composeResponse = function(status){
+Tweet.prototype.composeReply = function(status){
 	if(Array.isArray(status) === false){ status = [status]; }
-	var tags = _.reduce(status, function(tags, tag) { return tags + ' #' + tag; }, '');
-	return this.url + ' ' + tags + ' ' + this.user;
+	var tags = _(status).reduce(function(tags, tag){ return tags + ' #' + tag; }, '');
+	return _(this.links).reduce(function(links, link){ return links + ' ' + link; },'') + ' ' + tags + ' ' + this.user;
 };
-Tweet.prototype.respond = function(twitter, status){
-	twitter.updateStatus(this.composeResponse(status), {in_reply_to_status_id:this.id}, function(err, data){
-		if(err !== null){ throw err; }
+Tweet.prototype.reply = function(twitter, status){
+	twitter.updateStatus(this.composeReply(status), {in_reply_to_status_id:this.id}, function(err, data){
+		if(err) console.log('error while replying:\n' + err.message);
 	});
 };
+
+
+
+function process(tweet){
+	// save last seen id
+	fs.writeFileSync('state.json', JSON.stringify({lastSeenId:tweet.id}), 'utf8');
+	console.log('saw tweet: ' + tweet.id + (tweet.isStatusUpdate ? ' - status update':'')); 
+	
+	if(tweet.isStatusUpdate || !tweet.links.length === 0)	return;
+
+	// download the links from the message and respond when done
+	_(tweet.links)
+	.each(function(link){
+		var options = url.parse(link);
+		options.path = options.pathname;
+		var filename = path.basename(options.path);
+		console.log('downloading \'' + link + '\' to: ' + filename);
+		http.get(options, function(res){
+			switch(res.statusCode){
+				case 200:
+					res.on('end', function(){
+						tweet.reply(twitter, 'done');
+						console.log('downloading done - success');
+					});
+					var file = fs.createWriteStream(filename);
+					file
+						.once('open', function(fd){
+							util.pump(res, file, function(e){
+								tweet.reply(twitter, 'error');
+								console.log('error saving \'' + filename + '\':\n' + e.message);
+							});
+						});
+					break;
+				default:
+					tweet.respond(twitter, ['error', 'code' + res.statusCode]);
+					console.log('downloading done - unexpected status: ' + res.statusCode);
+			}
+		}).on('error', function(e){ console.log('downloading - error:\n' + e.message); });
+	});
+}
+
+
+var twitter = new Twitter(require('./options.js'));
+
+var state = { lastSeenId: null };
+state.lastSeenId = 146858815638339584;
+
+
+console.log('catching up...');
+var options = { since_id:state.lastSeenId, include_entities:true };
+twitter.getUserTimeline(options, function(err, data){
+	if(err) throw err;
+	console.log(data.length + ' tweet(s) to process');
+	_(data).each(function(item){ process(new Tweet(item)); });
+});
+
+console.log('listening...');
+twitter.stream('user', function(stream){
+	stream.on('data', function(data){
+		if(typeof data.id === 'undefined'){
+			console.log(sys.inspect(data));
+			return;
+		}
+		process(new Tweet(data));
+	});
+	stream.on('end', function(res){	console.log('listening ends'); });
+	stream.on('destroy', function(res){ console.log('listening stream destroyed'); });
+});
 
