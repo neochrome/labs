d8242ad8be8aabbde11ba0602644682d62bcb03e
neochrome <johan@stenqvist.net>
2012-03-12 23:25:23 +0100

    somewhat working chat rooms

--- rooms/server.js
+++ rooms/server.js
@@ -1,11 +1,51 @@
 var express = require('express');
 var app = express.createServer();
 var io = require('socket.io').listen(app);
+var _ = require('underscore');
 
 app.use('/', express.static(__dirname + '/public'));
 
-io.sockets.on('connection', function(socket){
-	socket.emit('rooms', ['lobby','room1','room2']);
+
+var clients = [];
+
+io.sockets.on('connection', function(client){
+	client.room = null;
+	client.name = 'anonymous';
+	clients.push(client);
+
+	publishRoomsList(client);
+
+	client.on('disconnect', function(){
+		clients.splice(clients.indexOf(client), 1);
+		publishRoomsList();
+	});
+
+	client.on('join', function(room){
+		client.room = room;
+		client.emit('room', client.room);
+		publishRoomsList();
+	});
+
+	client.on('message', function(text){
+		io.sockets.emit('message', { room: client.room, name: client.name, text: text });
+	});
+
 });
+
+var clientJoined = function(socket){
+};
+
+var publishRoomsList = function(socket){
+	var rooms = 
+		_.chain(clients)
+		.map(function(client){ return client.room; })
+		.reject(function(room){ return room === null; })
+		.uniq()
+		.value();
+	
+	socket = socket || io.sockets;
+	socket.emit('rooms', rooms);
+};
+
 
 app.listen(8181);
