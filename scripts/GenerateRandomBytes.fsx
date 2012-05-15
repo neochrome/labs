6fb28fe46a75c32d94fdb3e53e0eeccada476cf5
neochrome <johan@stenqvist.net>
2012-05-15 15:05:50 +0200

    added some old scripts

---
+++ scripts/GenerateRandomBytes.fsx
@@ -0,0 +1,9 @@
+﻿open System
+open System.Security
+open System.Security.Cryptography
+
+let rng = new RNGCryptoServiceProvider()
+let buffer = Array.create 32 0uy
+Console.WriteLine buffer.Length
+rng.GetBytes(buffer)
+buffer |> Array.iter (fun b -> Console.Write("{0:X2}", b))
---
+++ scripts/image_to_base64.fsx
@@ -0,0 +1,3 @@
+﻿let fileName = fsi.CommandLineArgs.[1]
+let mimeType = "image/" + System.IO.Path.GetExtension(fileName).ToLower()
+printfn "data:%s;base64,%s" mimeType (System.Convert.ToBase64String(System.IO.File.ReadAllBytes(fileName)))
